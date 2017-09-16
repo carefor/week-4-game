3cc75246d5d32674b6d1eb872fa89747bfd8c9b9
Caroline Forsyth <uncforsyth@gmail.com>
2017-09-16 01:27:19 -0400

    now just to redo css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,12 +33,13 @@ function crystalClick() {
 		$("#total-score").html(totalScore);
 		$("#turns").html(turnCounter);
 		console.log("The current score is " + totalScore);
-		if (totalScore === goalNumber) {
+			if (totalScore === goalNumber) {
 				gameWon();
 			} else if (totalScore > goalNumber) {
 				gameLost();
+			} else if (turnCounter === 0) {
+				noTurns();
 			}
-
 	});
 	$("#blue-crystal").on("click", function() {
 		totalScore += blueCrystal;
@@ -46,12 +47,13 @@ function crystalClick() {
 		$("#total-score").html(totalScore);
 		$("#turns").html(turnCounter);
 		console.log("The current score is " + totalScore);
-		if (totalScore === goalNumber) {
+			if (totalScore === goalNumber) {
 				gameWon();
 			} else if (totalScore > goalNumber) {
 				gameLost();
+			} else if (turnCounter === 0) {
+				noTurns();
 			}
-
 	});
 	$("#purple-crystal").on("click", function() {
 		totalScore += purpleCrystal;
@@ -59,12 +61,13 @@ function crystalClick() {
 		$("#total-score").html(totalScore);
 		$("#turns").html(turnCounter);
 		console.log("The current score is " + totalScore);
-		if (totalScore === goalNumber) {
+			if (totalScore === goalNumber) {
 				gameWon();
 			} else if (totalScore > goalNumber) {
 				gameLost();
+			} else if (turnCounter === 0) {
+				noTurns();
 			}
-
 	});
 	$("#white-crystal").on("click", function() {
 		totalScore += whiteCrystal;
@@ -76,8 +79,9 @@ function crystalClick() {
 				gameWon();
 			} else if (totalScore > goalNumber) {
 				gameLost();
+			} else if (turnCounter === 0) {
+				noTurns();
 			}
-
 	});
 }
 crystalClick();
@@ -86,39 +90,35 @@ function gameLost () {
 	alert("Your total score is too high.. You've succumbed to the crystal curse. Game over!");
 	lossCounter++;
 	$("#losses").html(lossCounter);
+	reset();
 }
+
+function noTurns() {
+	alert("Oops! Looks like you ran out of turns. Better luck next time.. ");
+	lossCounter++;
+	$("#losses").html(lossCounter);
+	reset();
+}
+//or if run out of turns
 
 function gameWon () {
 	alert("Your skills are legendary.. You win!");
 	winCounter++;
-	$("#wins").html(winCounter++);
+	$("#wins").html(winCounter);
+	reset();
 }
-
-
-
-//function for if you go above number and lose
-//or if run out of turns
-
-//	$(".crystal-button").on("click", function () {
-//		if (totalScore > goalNumber);
-//	});
-//}
-//gameLost();
-
-//function for if you ===the goal number and win
 
 function reset() {
         turnCounter = 20;
         $("#turns").html(turnCounter);
-        goalNumber = 0;
+        winCounter = 0;
+        $("#wins").html(winCounter);
+        lossCounter = 0;
+        totalScore = 0;
+        $("#total-score").html(totalScore);
+        $("#losses").html(lossCounter);
+         goalNumber = "";
         $("#goal-number").html(goalNumber);
         generateRandomAll(); 
-        var winCounter = 0;
-        $("#wins").html(winCounter);
-        var lossCounter = 0;
-        $("#losses").html(lossCounter);
-    }
-
-    reset();
-
-    //write in once game over immediately call reset function
+}
+reset();
